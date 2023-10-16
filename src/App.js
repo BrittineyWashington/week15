1e6e46c997dda58012a9299636c1f429f9683f34
BrittineyWashington <https://github.com>
2023-10-16 19:09:04 -0400

    2nd push added got-tv app code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function App() {
+  // My CRUD OPERATIONS HERE
+  //characters is storing everything in API
+  const [characters, setCharacters] = useState(['']); 
+
+  const url = 'https://game-of-thrones1.p.rapidapi.com/Characters';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '6f53b055dfmshfeabeeba9fa9381p173118jsnee5a73cee9c9',
+      'X-RapidAPI-Host': 'game-of-thrones1.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+
+function getCharacters() {
+  fetch(url)
+  .then(data => data.json())
+  .then(data => setCharacters(data))
 }
 
+useEffect(() => {
+  getCharacters()
+}, [])
+
+function deleteCharacter(id) {
+  fetch(`${url}/${id}`, {
+    method: "DELETE", 
+  }).then(() => getCharacters())
+}
+
+
+console.log('Hello World'); 
+
+  return (
+    <div className="App">
+      Hi Princess!
+      <h2>Characters: </h2>
+        {characters.map((character, index) => (
+          <div key={index}>
+            {/* This will be a container for all our info */}
+            <div>
+              <p>This is just a test! </p>
+            </div>
+          </div>
+        ))}
+    </div> 
+  )
+}
+      
 export default App;
